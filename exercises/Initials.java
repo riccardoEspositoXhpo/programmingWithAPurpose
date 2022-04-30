167ae6617b171b1f5705cf4e375309e6c9bd4986
Riccardo Esposito <espositoriccardo101@gmail.com>
2022-04-30 22:47:33 +0200

    adding exercises

---
+++ exercises/Initials.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class Initials {
+
+    public static void main(String[] args) {
+        System.out.println("***********           ***************");
+        System.out.println("************          ***************");
+        System.out.println("***      ***          **");
+        System.out.println("**     ***            **");
+        System.out.println("********              ************");
+        System.out.println("********              ************");
+        System.out.println("**   ***              **");
+        System.out.println("**    ***             **");
+        System.out.println("**     ***            ***************");
+        System.out.println("**       ***          ***************");
+    }
+
+}
